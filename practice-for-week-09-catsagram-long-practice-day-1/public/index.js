b457509eb8a84e75df42e4f5ae05a5c327da5866
Scot Nicol <scnicol@ucdavis.edu>
2022-07-23 13:41:06 -0700

    Add files via upload
    added the cats

--- practice-for-week-09-catsagram-long-practice-day-1/public/index.js
+++ practice-for-week-09-catsagram-long-practice-day-1/public/index.js
@@ -1,1 +1,24 @@
-// Your code here
+window.onload = () => {
+
+    const h1 = document.createElement('h1');
+    h1.innerText = 'Kitten Pic'
+    //console.log(h1)
+
+    const body = document.getElementsByTagName('body')[0];
+    body.appendChild(h1);
+
+    const getKitten = async () => {
+        const img = document.createElement('img');
+
+        const res = await fetch('https://api.thecatapi.com/v1/images/search');
+        //console.log(res);
+        const data = await res.json()
+
+        img.src = data[0].url;
+        //console.log(data);
+
+        body.appendChild(img);
+    }
+    getKitten();
+}
+
